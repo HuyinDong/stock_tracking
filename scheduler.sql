8f9983a1a471216a07b2c32a0dce848a02661bd6
Donghuyin <donghuyin@gmail.com>
2016-08-12 09:22:38 -0400

    add db

--- scheduler.sql
+++ scheduler.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: May 21, 2016 at 11:32 AM
--- Server version: 5.6.30-0ubuntu0.15.10.1
--- PHP Version: 5.6.11-1ubuntu3.3
+-- Generation Time: Aug 12, 2016 at 09:21 AM
+-- Server version: 5.6.31-0ubuntu0.15.10.1
+-- PHP Version: 5.6.11-1ubuntu3.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -23,20 +23,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `addclasses`
---
-
-CREATE TABLE IF NOT EXISTS `addclasses` (
-  `id` int(5) NOT NULL,
-  `pid` int(5) NOT NULL,
-  `course_id` int(5) NOT NULL,
-  `classroom_id` int(5) NOT NULL,
-  `time_id` int(5) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `classroom`
 --
 
@@ -45,7 +31,15 @@ CREATE TABLE IF NOT EXISTS `classroom` (
   `building` varchar(5) COLLATE utf8_bin NOT NULL,
   `room` varchar(5) COLLATE utf8_bin NOT NULL,
   `capacity` int(3) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Dumping data for table `classroom`
+--
+
+INSERT INTO `classroom` (`classroom_id`, `building`, `room`, `capacity`) VALUES
+(3, '42', '114A', 35),
+(4, '42', '115B', 30);
 
 -- --------------------------------------------------------
 
@@ -57,10 +51,21 @@ CREATE TABLE IF NOT EXISTS `course` (
   `course_id` int(5) NOT NULL,
   `prefix` varchar(10) COLLATE utf8_bin NOT NULL,
   `code` int(5) NOT NULL,
+  `name` varchar(30) COLLATE utf8_bin NOT NULL,
   `credits` int(3) NOT NULL,
   `section_prefix` varchar(10) COLLATE utf8_bin NOT NULL,
-  `numbers` int(3) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  `assigned` char(1) COLLATE utf8_bin NOT NULL DEFAULT 'N'
+) ENGINE=InnoDB AUTO_INCREMENT=4842 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Dumping data for table `course`
+--
+
+INSERT INTO `course` (`course_id`, `prefix`, `code`, `name`, `credits`, `section_prefix`, `assigned`) VALUES
+(4837, 'APCT', 408, 'Database', 3, 'sss-1', 'Y'),
+(4838, 'APCT', 408, 'Database', 3, 'sss-2', 'Y'),
+(4839, 'APCT', 407, 'Network', 3, 'ddd-1', 'Y'),
+(4841, 'APCT', 407, 'Network', 3, 'ddd-3', 'Y');
 
 -- --------------------------------------------------------
 
@@ -73,7 +78,42 @@ CREATE TABLE IF NOT EXISTS `professor` (
   `firstname` varchar(15) COLLATE utf8_bin NOT NULL,
   `lastname` varchar(15) COLLATE utf8_bin NOT NULL,
   `type` varchar(10) COLLATE utf8_bin NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=35 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Dumping data for table `professor`
+--
+
+INSERT INTO `professor` (`pid`, `firstname`, `lastname`, `type`) VALUES
+(19, 'Briana', 'Wellman', 'Full Time'),
+(20, 'Lily', 'Liang', 'Full Time'),
+(26, 'Anthony', 'Benitez', 'Adjunct'),
+(34, 'Li', 'Chen', 'Full Time');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `take`
+--
+
+CREATE TABLE IF NOT EXISTS `take` (
+  `id` int(5) NOT NULL,
+  `pid` int(5) NOT NULL,
+  `course_id` int(5) NOT NULL,
+  `cid` int(5) NOT NULL,
+  `sid` int(5) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Dumping data for table `take`
+--
+
+INSERT INTO `take` (`id`, `pid`, `course_id`, `cid`, `sid`) VALUES
+(11, 19, 4837, 3, 50),
+(13, 34, 4838, 4, 49),
+(15, 26, 0, 3, 52),
+(17, 34, 4841, 3, 52),
+(18, 20, 4839, -1, 51);
 
 -- --------------------------------------------------------
 
@@ -82,21 +122,25 @@ CREATE TABLE IF NOT EXISTS `professor` (
 --
 
 CREATE TABLE IF NOT EXISTS `time_slot` (
-  `time_id` int(3) NOT NULL,
-  `day` enum('Sunday','Monday','Tuesday','Wednsday','Thrusday','Friday','Saturday') COLLATE utf8_bin NOT NULL,
-  `start` datetime NOT NULL,
-  `end` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  `slot_id` int(3) NOT NULL,
+  `day` set('Su','M','T','W','TR','F','Sa') COLLATE utf8_bin NOT NULL,
+  `start` varchar(20) COLLATE utf8_bin NOT NULL,
+  `end` varchar(10) COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=53 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Dumping data for table `time_slot`
+--
+
+INSERT INTO `time_slot` (`slot_id`, `day`, `start`, `end`) VALUES
+(49, 'M,T,W', '10:00 AM', '11:30 AM'),
+(50, 'F', '2:00 PM', '3:30 PM'),
+(51, 'TR', '6:00 PM', '8:45 PM'),
+(52, 'T', '11:00 AM', '12:00 PM');
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `addclasses`
---
-ALTER TABLE `addclasses`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `classroom`
@@ -117,10 +161,16 @@ ALTER TABLE `professor`
   ADD PRIMARY KEY (`pid`);
 
 --
+-- Indexes for table `take`
+--
+ALTER TABLE `take`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `time_slot`
 --
 ALTER TABLE `time_slot`
-  ADD PRIMARY KEY (`time_id`);
+  ADD PRIMARY KEY (`slot_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -130,17 +180,27 @@ ALTER TABLE `time_slot`
 -- AUTO_INCREMENT for table `classroom`
 --
 ALTER TABLE `classroom`
-  MODIFY `classroom_id` int(5) NOT NULL AUTO_INCREMENT;
+  MODIFY `classroom_id` int(5) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `course`
+--
+ALTER TABLE `course`
+  MODIFY `course_id` int(5) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4842;
 --
 -- AUTO_INCREMENT for table `professor`
 --
 ALTER TABLE `professor`
-  MODIFY `pid` int(5) NOT NULL AUTO_INCREMENT;
+  MODIFY `pid` int(5) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=35;
+--
+-- AUTO_INCREMENT for table `take`
+--
+ALTER TABLE `take`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=19;
 --
 -- AUTO_INCREMENT for table `time_slot`
 --
 ALTER TABLE `time_slot`
-  MODIFY `time_id` int(3) NOT NULL AUTO_INCREMENT;
+  MODIFY `slot_id` int(3) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=53;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
